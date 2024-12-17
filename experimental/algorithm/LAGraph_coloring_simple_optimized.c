c3008651f672b09cf68c090230f7ba3256b81750
Tim Davis <DrTimothyAldenDavis@gmail.com>
2024-12-17 17:22:48 -0600

    updates from Dec 17 meeting

--- experimental/algorithm/LAGraph_coloring_simple_optimized.c
+++ experimental/algorithm/LAGraph_coloring_simple_optimized.c
@@ -2,6 +2,12 @@
 #include "LAGraphX.h"
 // add this algorithm to LAGraphX.h
 
+#define LG_FREE_ALL                 \
+    GrB_free (&local_color) ;       \
+    GrB_free (&weight) ;            \
+    GrB_free (&in_curr_subset) ;    \
+    GrB_free (&max_weights) ;
+
 int LAGraph_coloring_simple_optimized
 (
     // output
@@ -14,39 +20,38 @@ int LAGraph_coloring_simple_optimized
 )
 {
     bool verbose = false;
+    GrB_Vector local_color = NULL;
+    GrB_Vector weight = NULL;
+    GrB_Vector in_curr_subset = NULL;
+    GrB_Vector max_weights = NULL;
 
     GrB_Index n;
-    GrB_Matrix_nrows(&n, G->A);
+    GRB_TRY (GrB_Matrix_nrows(&n, G->A)) ;
+
+    GrB_Type Int = (n < UINT32_MAX) ? GrB_UINT32 : GrB_UINT64 ;
 
     /* initialize local copy of color to SPARSE vector */
-    GrB_Vector local_color = NULL;
-    GRB_TRY(GrB_Vector_new(&local_color, GrB_INT32, n));
+    GRB_TRY(GrB_Vector_new(&local_color, Int, n));
 
     // lg_set_format_hint -> bitmap
 
     /* weights initialized randomly
     *  seed of 20 was chosen arbitrarily */   
-    GrB_Vector weight = NULL;
     GRB_TRY(GrB_Vector_new(&weight, GrB_UINT64, n));
     GRB_TRY(GrB_assign (weight, NULL, NULL, 0, GrB_ALL, n, NULL));
     LAGraph_Random_Seed(weight, 20, msg);
 
-    GrB_Vector in_curr_subset = NULL;
     GRB_TRY(GrB_Vector_new(&in_curr_subset, GrB_BOOL, n));
 
-    GrB_Vector max_weights = NULL;
     GRB_TRY(GrB_Vector_new(&max_weights, GrB_UINT64, n));
 
-    GrB_Scalar reduced_scalar = NULL;
-    GrB_Scalar_new(&reduced_scalar, GrB_INT32);
-
-    GrB_Scalar color_scalar = NULL;
-    GrB_Scalar_new(&color_scalar, GrB_INT32);
-    
     /* algorithm start */
-    for (int curr_color = 1; curr_color < n+1; curr_color++) {
+    for (int64_t curr_color = 1; curr_color < n+1; curr_color++) {
         /* mxv - find maximum of all neighboring weights */
-        GRB_TRY(GrB_mxv(max_weights, local_color, GrB_NULL, GrB_MAX_SECOND_SEMIRING_UINT64, G->A, weight, GrB_DESC_RSC));
+
+        // FIXME: try using a set of sparse candidate nodes, not yet colored
+        GRB_TRY(GrB_mxv(max_weights, local_color, GrB_NULL,
+            GrB_MAX_SECOND_SEMIRING_UINT64, G->A, weight, GrB_DESC_RSC));
 
         /* eWiseAdd - 1 if current weight > max neighboring weight */
         GRB_TRY(GrB_eWiseMult(in_curr_subset, GrB_NULL, GrB_NULL, GrB_GT_UINT64, weight, max_weights, GrB_NULL));
@@ -54,9 +59,12 @@ int LAGraph_coloring_simple_optimized
         GRB_TRY(GrB_select(in_curr_subset, GrB_NULL, GrB_NULL, GrB_VALUEEQ_BOOL, in_curr_subset, true, GrB_NULL));
 
         /* reduce - OR all entries in in_curr_subset - if false, break */
+        // FIXME: just check nvals(in_curr_subset)
         bool subset_exists;
         GRB_TRY(GrB_reduce(&subset_exists, GrB_NULL, GrB_LOR_MONOID_BOOL, in_curr_subset, GrB_NULL));
         if (subset_exists == false) { break; }
+
+        // FIXME: future: if in_curr_subset is empty, but nvals (local_color) < n, then BROKEN
 
         /* assign - write current color to C vector according to in_curr_subset mask */
         GRB_TRY(GrB_assign(local_color, in_curr_subset, GrB_NULL, curr_color, GrB_ALL, n, GrB_DESC_S));
@@ -66,6 +74,7 @@ int LAGraph_coloring_simple_optimized
     }
 
     (*color) = local_color;
-
+    local_color = NULL ;
+    LG_FREE_ALL ;
     return (GrB_SUCCESS) ;
 }
