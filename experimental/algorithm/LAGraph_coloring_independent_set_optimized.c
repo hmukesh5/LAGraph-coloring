4e4d2e796b31036bc644deed5c7b8058c54f31b8
Hemanth <hmukesh@outlook.com>
2025-02-05 16:06:59 -0600

    Update LAGraph_coloring_independent_set_optimized.c

--- experimental/algorithm/LAGraph_coloring_independent_set_optimized.c
+++ experimental/algorithm/LAGraph_coloring_independent_set_optimized.c
@@ -46,6 +46,8 @@ int LAGraph_coloring_independent_set_optimized
     GRB_TRY(GrB_assign (weight, NULL, NULL, 0, GrB_ALL, n, NULL));
     LG_TRY(LAGraph_Random_Seed(weight, 20, msg));
 
+    printf("random done\n");
+
     GRB_TRY(GrB_Vector_new(&in_curr_subset, GrB_BOOL, n));
 
     GRB_TRY(GrB_Vector_new(&max_weights, GrB_UINT64, n));
